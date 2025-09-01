0a7aea7d8391f7b8c390ff2d2728242ab584504f
Stefan Tumey <stefan.tumey@gmail.com>
2025-08-31 22:12:46 -0400

    fix type check errors

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import { DayPicker, DayPickerProps, CustomComponents } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -18,7 +18,8 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+        months:
+          "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
@@ -52,11 +53,16 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+        // Correct property names for v9
+        PreviousMonthButton: (props: React.ComponentProps<"button">) => (
+          <button {...props}>
+            <ChevronLeft className={cn("h-4 w-4", props.className)} />
+          </button>
         ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+        NextMonthButton: (props: React.ComponentProps<"button">) => (
+          <button {...props}>
+            <ChevronRight className={cn("h-4 w-4", props.className)} />
+          </button>
         ),
       }}
       {...props}
